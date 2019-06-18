571e34826897419160504080ba529b981c0101c2
kostia7alania <osetia-alania@mail.ru>
2019-06-18 21:04:01 +0300

    UPDATE FROM 2019 --> MOS.RU  автоматизация подачи ЯРМАРКА ВЫХОДНОГО ДНЯ  Москва (скрипт) 2019

---
+++ test2k19.js
@@ -0,0 +1,8 @@
+﻿const zayavitel = 'Индивидуальный предприниматель'
+
+// Категория заявителя
+iim(`TAG POS=1 TYPE=LABEL FORM=ID:form_element ATTR=TXT:${zayavitel.replace(/ /gim,'<SP>')}`)
+
+
+
+function iim (e) {return iimPlayCode(e)}
